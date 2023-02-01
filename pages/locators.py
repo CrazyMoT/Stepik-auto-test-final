35ec964b4558d4270f25e7d4891e9367fdd5c2a7
Crazy_MoT <matgol1998@gmail.com>
2023-02-01 23:54:24 +0530

    Add new tests

--- pages/locators.py
+++ pages/locators.py
@@ -11,8 +11,14 @@ class BookPageLocators():
     BOOK_PRICE = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
     BOOK_NAME_PAGE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
     BOOK_PRICE_PAGE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
 
 
 class LoginPageLocators():
     REG = (By.ID, "register_form")
     LOGIN = (By.ID, "login_form")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -5,7 +5,7 @@ from .base_page import BasePage
 from .locators import BookPageLocators
 
 
-class BookPage(BasePage):
+class ProductPage(BasePage):
     def should_be_book_to_basket(self):
         WebDriverWait(self.browser, 20).until(
             EC.element_to_be_clickable((BookPageLocators.BASKET))).click(), "Error locate button"
@@ -17,3 +17,11 @@ class BookPage(BasePage):
         time.sleep(1)
         assert self.browser.find_element(*BookPageLocators.BOOK_PRICE).text == \
                self.browser.find_element(*BookPageLocators.BOOK_PRICE_PAGE).text, "Error price"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*BookPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_disappeared(self):
+        assert self.is_not_element_present(*BookPageLocators.SUCCESS_MESSAGE), \
+            "Success message is disappeared"
